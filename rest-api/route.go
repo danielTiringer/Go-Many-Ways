52f0fe36e54ca2be0aeb1204bcba018cb2683ac2
danielTiringer <tiringerdaniel@gmail.com>
2020-05-03 21:58:24 +0200

    feat(api): implement add posts

--- rest-api/route.go
+++ rest-api/route.go
@@ -34,5 +34,26 @@ func getPosts(w http.ResponseWriter, r *http.Request) {
 }
 
 func addPost(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-type", "application/json")
+	var post Post
+	err := json.NewDecoder(r.Body).Decode(&post)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(`{ "error": "Error unmashalling the request." }`))
+		return
+	}
 
+	post.Id = len(posts) + 1
+	posts = append(posts, post)
+
+
+	result, err := json.Marshal(posts)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(`{ "error": "Error mashalling the posts array." }`))
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+	w.Write(result)
 }
