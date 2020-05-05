9d6e9fb153bf78461bd33626fb69e2ddadf63bf9
danielTiringer <tiringerdaniel@gmail.com>
2020-05-05 06:33:22 +0200

    feat(tests): add mocking and mock findall

--- rest-api/service/post-service_test.go
+++ rest-api/service/post-service_test.go
@@ -4,9 +4,26 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
 
 	"github.com/danielTiringer/Go-Many-Ways/rest-api/entity"
 )
+
+type MockRepository struct {
+	mock.Mock
+}
+
+func (mock *MockRepository) FindAll() ([]entity.Post, error) {
+	args := mock.Called()
+	result := args.Get(0)
+	return result.([]entity.Post), args.Error(1)
+}
+
+func (mock *MockRepository) Save(post *entity.Post) (*entity.Post, error) {
+	args := mock.Called()
+	result := args.Get(0)
+	return result.(*entity.Post), args.Error(1)
+}
 
 func TestValidateEmptyPost(t *testing.T) {
 	testService := NewPostService(nil)
@@ -26,4 +43,23 @@ func TestValidateEmptyPostTitle(t *testing.T) {
 
 	assert.NotNil(t, err)
 	assert.Equal(t, "The post title is empty.", err.Error())
+}
+
+func TestFindAll(t *testing.T) {
+	mockRepo := new(MockRepository)
+
+	var identifier int64 = 1
+	post := entity.Post{ID: identifier, Title: "Test title", Text: "Test text"}
+	mockRepo.On("FindAll").Return([]entity.Post{post}, nil)
+
+	testService := NewPostService(mockRepo)
+
+	result, _ := testService.FindAll()
+
+	mockRepo.AssertExpectations(t)
+
+	assert.Equal(t, 1, len(result))
+	assert.Equal(t, identifier, result[0].ID)
+	assert.Equal(t, "Test title", result[0].Title)
+	assert.Equal(t, "Test text", result[0].Text)
 }
